79934b800082d81da0fac3a33b6ab6ac341e9e11
Md Firoz Islam <156815739+Firoz587@users.noreply.github.com>
2024-04-19 03:25:38 +0600

    Add files via upload
    added week-4 day-5 code

--- week-4/day-5/Possible_or_Not.cpp
+++ week-4/day-5/Possible_or_Not.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int b;
+        cin>>b;
+        int cur=-1;
+        int i=0;
+        while(i<n)
+        {
+            int x;
+            cin>>x;
+            if((x&b)==b)
+                cur &= x;
+            i++;
+        }
+        if(cur==b)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
 
