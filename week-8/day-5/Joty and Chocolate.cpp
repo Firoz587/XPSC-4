68b269aeaaeed7a766205f6315ee203e0240710a
Md Firoz Islam <156815739+Firoz587@users.noreply.github.com>
2024-05-15 23:26:03 +0600

    Add files via upload
    added_week-8_day-5_codes

---
+++ week-8/day-5/Joty and Chocolate.cpp
@@ -0,0 +1,34 @@
+/******** Name :- Md Firoz Islam ***********
+University :- Northern University Bangladesh
+********** Dhaka,Bangladesh ***************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll gcd(ll a ,ll b)
+{
+    return __gcd(a,b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return ((a/gcd(a,b))*b);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,a,b,p,q,ans=0,overlap;
+    cin>>n>>a>>b>>p>>q;
+
+    ans+=((n/a)*p);
+    ans+=((n/b)*q);
+    overlap = (n/lcm(a,b));
+    ans-=(overlap*(p+q));
+    ans+=(overlap* max(p,q));
+
+    cout<<ans<<"\n";
+    return 0;
+}
